d3b0d150b58ccd3d407dc9eb79b41370981621ef
Vinodh <vinodh.jeevanantham@gmail.com>
2024-06-28 15:47:09 +0530

    kv implemented

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export interface Env {
   GOOGLE_PRIVATE_KEY: string;
   GOOGLE_CLIENT_EMAIL: string;
   GOOGLE_TOKEN_URI: string;
+  GKEY_KV_NAMESPACE: KVNamespace;
 }
 
 // Define the enum for sync_type
@@ -69,14 +70,44 @@ export default {
         return new Response('Invalid sync type', { status: 400 });
       }
 
+      const kv_value = await env.GKEY_KV_NAMESPACE.get('GOOGLE_PRIVATE_KEY');
+      if (!kv_value) {
+        return new Response('Missing kv credentials', { status: 400 });
+      }
+
+      const private_key = JSON.parse(kv_value);
+
       const auth = await fetchAccessToken({
-        GOOGLE_PRIVATE_KEY: env.GOOGLE_PRIVATE_KEY,
+        GOOGLE_PRIVATE_KEY: private_key,
         GOOGLE_CLIENT_EMAIL: env.GOOGLE_CLIENT_EMAIL,
         GOOGLE_TOKEN_URI: env.GOOGLE_TOKEN_URI,
       });
 
       try {
         return manualD1GoogleSync({ request, datasources, SHEET_ID: env.SHEET_ID, SHEET_API_KEY: env.SHEET_API_KEY, auth, sync_type });
+      } catch (error) {
+        return new Response(`Failed to sync : ${error}`, { status: 500 });
+      }
+    }
+
+    if (url.pathname === '/kv-config' && request.method === 'POST') {
+      const token = request.headers.get('Authorization');
+      if (!token || token !== env.SYNC_TOKEN) {
+        return new Response('Unauthorized', { status: 401 });
+      }
+
+      try {
+        const data = await request.json();
+        if (!data) {
+          return new Response('Missing data', { status: 400 });
+        }
+        const private_key = JSON.stringify(data.gkey);
+        await env.GKEY_KV_NAMESPACE.put('GOOGLE_PRIVATE_KEY', private_key);
+        const value = await env.GKEY_KV_NAMESPACE.get('GOOGLE_PRIVATE_KEY');
+        if (value === null) {
+          return new Response('Value not found', { status: 404 });
+        }
+        return new Response('KV config updated', { status: 200 });
       } catch (error) {
         return new Response(`Failed to sync : ${error}`, { status: 500 });
       }
